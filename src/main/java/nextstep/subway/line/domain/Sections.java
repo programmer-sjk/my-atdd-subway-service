e8d83ce61daa0d9bec0c50c32378fb1ebe127946
sjk5766 <wjdrnr6662@gmail.com>
2022-11-22 16:31:27 +0900

    feat(domain): 노선 추가 기능 domain 함수 추가

--- src/main/java/nextstep/subway/line/domain/Sections.java
+++ src/main/java/nextstep/subway/line/domain/Sections.java
@@ -4,9 +4,12 @@ import nextstep.subway.station.domain.Station;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
+import javax.persistence.NoResultException;
 import javax.persistence.OneToMany;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Embeddable
@@ -27,14 +30,72 @@ public class Sections {
     }
 
     public List<Station> getStations() {
+        if (sections.isEmpty()) {
+            return new ArrayList<>();
+        }
+
+        Map<Station, Station> stations = sectionsToMap();
+        return sortStations(stations, findUpStation(stations));
+    }
+
+    private Map<Station, Station> sectionsToMap() {
         return sections.stream()
-                .map(Section::getStations)
-                .flatMap(Collection::stream)
-                .distinct()
-                .collect(Collectors.toList());
+                .collect(Collectors.toMap(Section::getUpStation, Section::getDownStation));
+    }
+
+    private Station findUpStation(Map<Station, Station> stations) {
+        return stations.keySet()
+                .stream()
+                .filter(upStation -> !stations.containsValue(upStation))
+                .findFirst()
+                .orElseThrow(() -> new NoResultException("상행 종착역이 존재하지 않습니다."));
+    }
+
+    private List<Station> sortStations(Map<Station, Station> stations, Station upStation) {
+        List<Station> sortedStations = new ArrayList<>();
+
+        Station currentStation = upStation;
+        while (currentStation != null) {
+            sortedStations.add(currentStation);
+            currentStation = stations.get(currentStation);
+        }
+
+        return sortedStations;
     }
 
     public void add(Section newSection) {
+        if (sections.isEmpty()) {
+            sections.add(newSection);
+            return;
+        }
+
+        validateAddition(newSection);
+        sections.forEach(section -> section.update(newSection));
         sections.add(newSection);
+    }
+
+    private void validateAddition(Section section) {
+        validateHasBothStations(section);
+        validateHasNotBothStations(section);
+    }
+
+    private void validateHasBothStations(Section section) {
+        if (getStations().containsAll(section.getStations())) {
+            throw new IllegalArgumentException("이미 등록된 구간 입니다.");
+        }
+    }
+
+    private void validateHasNotBothStations(Section section) {
+        if (hasNotBothStations(section)) {
+            throw new IllegalArgumentException("등록할 수 없는 구간 입니다.");
+        }
+    }
+
+    private boolean hasNotBothStations(Section section) {
+        List<Station> stations = getStations();
+
+        return section.getStations()
+                .stream()
+                .noneMatch(stations::contains);
     }
 }
