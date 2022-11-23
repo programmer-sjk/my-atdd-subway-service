4b0219eb2dcea342b32cdb44f997f176cc99e94f
sjk5766 <wjdrnr6662@gmail.com>
2022-11-23 16:42:09 +0900

    refactor: 코드 리뷰 반영

--- src/test/java/nextstep/subway/line/domain/SectionTest.java
+++ src/test/java/nextstep/subway/line/domain/SectionTest.java
@@ -1,7 +1,5 @@
 package nextstep.subway.line.domain;
 
-import nextstep.subway.line.domain.Distance;
-import nextstep.subway.line.domain.Section;
 import nextstep.subway.station.domain.Station;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,6 +23,36 @@ class SectionTest {
 
         // then
         assertThat(section.getUpStation()).isEqualTo(newStation);
+    }
+
+    @DisplayName("지하철 역이 구간의 상행선과 동일한지 알 수 있다")
+    @Test
+    void isEqualUpStation() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("강남역");
+        Section section = new Section(upStation, downStation, new Distance(10));
+
+        // when
+        boolean result = section.isEqualUpStation(upStation);
+
+        // then
+        assertThat(result).isTrue();
+    }
+
+    @DisplayName("지하철 역이 구간의 하행선과 동일한지 알 수 있다")
+    @Test
+    void isEqualDownStation() {
+        // given
+        Station upStation = new Station("판교역");
+        Station downStation = new Station("강남역");
+        Section section = new Section(upStation, downStation, new Distance(10));
+
+        // when
+        boolean result = section.isEqualDownStation(downStation);
+
+        // then
+        assertThat(result).isTrue();
     }
 
     @DisplayName("하행선을 바꿀 수 있다.")
