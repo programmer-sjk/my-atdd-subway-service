10ca13f4d837aa84b47d6c1a8f4501b7528de868
sjk5766 <wjdrnr6662@gmail.com>
2022-11-29 11:34:59 +0900

    factor: Lines 최대 추가요금 구하는 도메인 로직 및 테스트 작성

--- src/main/java/nextstep/subway/line/domain/Lines.java
+++ src/main/java/nextstep/subway/line/domain/Lines.java
@@ -2,6 +2,8 @@ package nextstep.subway.line.domain;
 
 import nextstep.subway.station.domain.Station;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -9,7 +11,7 @@ public class Lines {
     private final List<Line> lines;
 
     public Lines(List<Line> lines) {
-        this.lines = lines;
+        this.lines = new ArrayList<>(lines);
     }
 
     public List<Station> getStations() {
@@ -29,5 +31,12 @@ public class Lines {
     public boolean hasNotStation(Station station) {
         return lines.stream()
                 .noneMatch(line -> line.getStations().contains(station));
+    }
+
+    public ExtraFare maxExtraFare() {
+        return lines.stream()
+                .map(Line::getExtraFare)
+                .max(Comparator.comparingInt(ExtraFare::get))
+                .orElse(new ExtraFare(ExtraFare.FREE));
     }
 }
